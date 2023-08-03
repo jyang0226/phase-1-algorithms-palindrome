3aedf4a6d82d0429f8e0b129ee77f0ef511d7910
Jaden Falbee <j.falbee04@gmail.com>
2023-08-02 21:05:22 -0400

    finishes lab

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  let lWord = word.toLowerCase();
+  let lWordRev = lWord.split('').reverse().join('');
+  return lWord === lWordRev;
 }
 
 /* 
   Add your pseudocode here
+  1. change all word strings into lowercase so palindrome can run with no problem.
+  2. make it so spaces arent considered in the name //=> "taco cat" >/ "tac ocat" and instead would return "tacocat"
+  3. split string by individual letters, reverse string, and join the letters again.
+  4. see if palindrome works
 */
 
 /*
@@ -20,6 +27,11 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("taco cat"));
 }
 
 module.exports = isPalindrome;
